93ce3a1572c136bc19a0b10f312ce9d7fd59ec09
vivek <sonagravivek1@gmail.com>
2023-12-13 11:13:26 +0530

    add navbar

--- src/componet/Navbar.js
+++ src/componet/Navbar.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <>Hello would</>;
+  return (
+    <>
+      {" "}
+      <nav className="bg-gray-800 p-4">
+        <div className="container mx-auto">
+          <Link to="/" className="text-white font-bold text-xl">
+            Your Logo
+          </Link>
+          <div className="flex">
+            <Link to="/" className="text-white mx-4">
+              Home
+            </Link>
+            <Link to="/about" className="text-white mx-4">
+              About
+            </Link>
+            <Link to="/contact" className="text-white mx-4">
+              Contact
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
