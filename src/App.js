e48df72864919162e79c76aa84a12beece010eae
Sachin9388 <sonagrasachin9687@gmail.com>
2023-12-15 14:57:36 +0530

    Delete node Modules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./componet/Navbar";
-const Home = () => <div>Home Page</div>;
-const About = () => <div>About Page</div>;
-const Contact = () => <div>Contact Page</div>;
+const Home = () => (
+  <div className="font-bold text-4xl my-4 text-gray-700 text-center">
+    Home Page
+  </div>
+);
+const About = () => (
+  <div className="font-bold text-4xl my-4 text-gray-700 text-center">
+    About Page
+  </div>
+);
+const Contact = () => (
+  <div className="font-bold text-4xl my-4 text-gray-700 text-center">
+    Contact Page
+  </div>
+);
 
 function App() {
   return (
