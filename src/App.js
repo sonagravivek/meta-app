d6d3cfe9a9e4dba53e21c44512843d75e5c255b4
kirtan <sonagrakirtan@gmail.com>
2023-12-18 12:40:00 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import "./App.css";
+
 import MainApp from "./app/index";
 import { BrowserRouter as Router } from "react-router-dom";
+
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./componet/Navbar";
+const Home = () => (
+  <div className="font-bold text-4xl my-4 text-gray-700 text-center">
+    Home Page
+  </div>
+);
+const About = () => (
+  <div className="font-bold text-4xl my-4 text-gray-700 text-center">
+    About Page
+  </div>
+);
+const Contact = () => (
+  <div className="font-bold text-4xl my-4 text-gray-700 text-center">
+    Contact Page
+  </div>
+);
+
 
 function App() {
   return (
