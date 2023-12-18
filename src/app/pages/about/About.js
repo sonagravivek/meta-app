3b697ceb17de03b517bea0f22edd6d6a2bcf25ec
kirtan <sonagrakirtan@gmail.com>
2023-12-18 12:32:11 +0530

    add about new

--- src/app/pages/about/About.js
+++ src/app/pages/about/About.js
@@ -3,7 +3,8 @@ import React from 'react'
 function About() {
     return ( 
         <>
-        <h1>this is About kirtan page </h1>
+            <h1>this is About kirtan page </h1>
+            <h1>this is about page</h1>
         </>
      );
 }
